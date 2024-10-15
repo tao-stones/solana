107d6b2bd7f099e382d728a1889572c2b1ba5a94
Tao Zhu <tao@anza.xyz>
2024-10-15 16:28:20 -0500

    restore CB program default cost

--- programs/compute-budget/src/lib.rs
+++ programs/compute-budget/src/lib.rs
@@ -1,7 +1,6 @@
 use solana_program_runtime::declare_process_instruction;
 
-// compute budget instructions are processed before VM, therefore do not have execution cost
-pub const DEFAULT_COMPUTE_UNITS: u64 = 0;
+pub const DEFAULT_COMPUTE_UNITS: u64 = 150;
 
 declare_process_instruction!(Entrypoint, DEFAULT_COMPUTE_UNITS, |_invoke_context| {
     // Do nothing, compute budget instructions handled by the runtime
